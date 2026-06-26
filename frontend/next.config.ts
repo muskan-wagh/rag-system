5756bbeb1f8fb431164bb02f6a4bb959920ec13c
Muskan <muskanwagh1608@gmail.com>
2026-06-26 17:29:13 +0530

    checking the pipeline

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://rag-system-backend.up.railway.app/"
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}api/:path*`,
+        destination: `${API_URL}api/:path*`,
       },
     ];
   },
